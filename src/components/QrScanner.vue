f9d5bf034cef363d81066147747bebefb01b6ab3
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-10-15 19:15:09 -0300

    remove unused styles

--- src/components/QrScanner.vue
+++ src/components/QrScanner.vue
@@ -63,27 +63,4 @@ function turnCameraOff() {
 h2 {
   margin: 0 0 20px;
 }
-
-.validation-success, .validation-failure, .validation-pending {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-
-  background-color: rgba(255, 255, 255, .8);
-  text-align: center;
-  font-weight: bold;
-  font-size: 1.4rem;
-  padding: 10px;
-
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: center
-
-}
-.validation-success {
-  color: green;
-}
-.validation-failure {
-  color: red;
-}
 </style>
